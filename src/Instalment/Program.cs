746c8e5c582e31f009ba43092e94266d380fc19f
Måns <mj@baase.com>
2023-06-12 02:53:25 +0200

    add instalment example app

---
+++ src/Instalment/Program.cs
@@ -0,0 +1,22 @@
+using Instalment.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<HomeController>();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
+
+app.Run();
