ecdb2c5679772fae3c694c94c938808ca2fb6843
Måns <mj@baase.com>
2023-09-05 13:20:31 +0200

    add bootstrap

--- src/Instalment/Controllers/HomeController.cs
+++ src/Instalment/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ public class HomeController : Controller
                     Reference = "123456789",
                     Amount = 3000,
                     PersonalNumber = "9001010017",
-                    CallbackUrl = "https://localhost:7265/callback"
+                    CallbackUrl = "https://your-callback-url"
                 });
 
         response.EnsureSuccessStatusCode();
